7d8f71d927dd6ee5bfc9005ec3e7dab41d77e139
Victoria <victoriasnow1124@gmail.com>
2014-11-03 22:48:29 -0800

    proj2-2

--- CS 61C/PuzzleSolver/SlidingBfsSpark.py
+++ CS 61C/PuzzleSolver/SlidingBfsSpark.py
@@ -1,152 +1,63 @@
 from pyspark import SparkContext
-<<<<<<< HEAD
-import Sliding, argparse, math
-def deserializeData(value):
-    return tuple(value)
-
-def serializeData(value):
-    return ''.join(value)
-
-def bfs_flat_map(value):
-    """ YOUR CODE HERE """
-    return [Sliding.board_to_hash(WIDTH, HEIGHT, child) for child in Sliding.children(WIDTH,HEIGHT,Sliding.hash_to_board(WIDTH, HEIGHT, value[0]))]
-
-# def gen_bfs_flat_map(l):
-#     return lambda value: [value] + [(serializeData(child),l) for child in Sliding.children(WIDTH,HEIGHT,deserializeData(value[0]))] if value[1] == l - 1 else [value]
-
-# def bfs_map(value):
-#     return (value, level)
-=======
 import Sliding, argparse
 
 
 def bfs_flat_map(value):
     """ YOUR CODE HERE """
-    childrenLst=[]    
-    childrenLst.append(value)
-    for child in Sliding.children(WIDTH,HEIGHT,value[0]):
-        pair=[]
-        pair.append(tuple(child))
-        pair.append(level)     
-        childrenLst.append(tuple(pair))
+    # childrenLst=[]    
+    # childrenLst.append(value)
+    # for child in Sliding.children(WIDTH,HEIGHT,Sliding.hash_to_board(WIDTH, HEIGHT, value[0])):
+    #     pair=[]
+    #     pair.append(Sliding.board_to_hash(WIDTH, HEIGHT, child))
+    #     pair.append(level)     
+    #     childrenLst.append(tuple(pair))
        
-    return childrenLst
-    
+    # return childrenLst
+
+    childrenLst = []
+    childrenLst.append((value[0], value[1]))
+    if(value[1] == level - 1):
+        for child in Sliding.children(WIDTH, HEIGHT, Sliding.hash_to_board(WIDTH,HEIGHT, value[0])):
+            childrenLst.append((Sliding.board_to_hash(WIDTH,HEIGHT,child), value[1]+1))
+    return childrenLst 
 def bfs_map(value):
     """ YOUR CODE HERE """
-
-    pass
->>>>>>> FETCH_HEAD
-
+    return (value,level)
 
 def bfs_reduce(value1, value2):
     """ YOUR CODE HERE """
     return min(value1,value2)
 
-<<<<<<< HEAD
-=======
-def solve_sliding_puzzle(master, output, height, width):
-    """
-    Solves a sliding puzzle of the provided height and width.
-     master: specifies master url for the spark context
-     output: function that accepts string to write to the output file
-     height: height of puzzle
-     width: width of puzzle
-    """
-    # Set up the spark context. Use this to create your data
-    sc = SparkContext(master, "python")
->>>>>>> FETCH_HEAD
-
-
 def solve_puzzle(master, output, height, width, slaves):
     global HEIGHT, WIDTH, level
     HEIGHT=height
     WIDTH=width
-<<<<<<< HEAD
     level = 0
-=======
-    level = 0 # this "constant" will change, but it remains constant for every MapReduce job
-
-    # The solution configuration for this sliding puzzle. You will begin exploring the tree from this node
-    sol = Sliding.solution(WIDTH, HEIGHT)
->>>>>>> FETCH_HEAD
 
     sc = SparkContext(master, "python")
 
     """ YOUR CODE HERE """
+    """ YOUR MAP REDUCE PROCESSING CODE HERE """
     solution=Sliding.solution(WIDTH, HEIGHT)
     sol = Sliding.board_to_hash(WIDTH, HEIGHT, solution)
-    """ YOUR MAP REDUCE PROCESSING CODE HERE """
-<<<<<<< HEAD
-    # dataAggregate = sc.parallelize([(serializeData(sol), 0)]);
-    dataAggregate = sc.parallelize([(sol, 0)]);
-    data = dataAggregate
-    curLen = 1
-
-    #addFunc = lambda x, y: x + y
-    maximum = math.factorial(HEIGHT*WIDTH)/2
-    k=0 #count number
-    p=0 #partition number
-    if (maximum<13):
-        k=16
-        p=8
-    elif(maximum<362880):
-        k=32
-        p=8
-    else:
-        k=56
-        p=16
-    while curLen < maximum: 
-
-        data = data.flatMap(bfs_flat_map) \
-                   .distinct() \
-                   .map(gen_bfs_map(level))
-        dataAggregate += data
-        level += 1
-        if(level%k == 0):
-            data = dataAggregate.partitionBy(p).reduceByKey(bfs_reduce)
-            dataAggregate = data
-            curLen = data.count()
-    # data.coalesce(6).saveAsTextFile(output)
-    pairs = data.collect()
-    # pairs=sorted(pairs, key = lambda value : value[1]) 
-
-=======
-    parent = [(sol,level),]
-    data = sc.parallelize(parent)
+    data = sc.parallelize([(sol,level),])
     counter = 0
     curLen = 1 
     while(counter < curLen):
         level += 1
         data = data.flatMap(bfs_flat_map)
         
-        if (level % 8 ==0):
-            data = data.partitionBy(4).reduceByKey(bfs_reduce)
 
-        if (level% 32 == 0):
-            data = data.partitionBy(16).reduceByKey(bfs_reduce)
-
-        if (level% 4 == 0):
+        if (level% 12 == 0):
+            data = data.partitionBy(PARTITION_COUNT)
+        data = data.reduceByKey(bfs_reduce)
+        if (level% 6 == 0):
             counter = curLen
             curLen = data.count()
-        data = data.reduceByKey(bfs_reduce)
-
-    pairs = data.collect()
-    pairs=sorted(pairs, key = lambda value : value[1]) 
-
+        
+        
     """ YOUR OUTPUT CODE HERE """
-    for pair in pairs:
-        string=str(pair[1])+" "+str(pair[0])
-        output(string)
->>>>>>> FETCH_HEAD
-
-    """ YOUR OUTPUT CODE HERE """
-    # pairs.coalesce(6).saveAsTextFile(output)
-    # print(pairs)
-    for pair in pairs:
-        # print (pair)
-        string=str(pair[1])+" "+str(tuple(pair[0]))
-        output(string)
+    data.coalesce(slaves).saveAsTextFile(output)
     sc.stop()
 
 
